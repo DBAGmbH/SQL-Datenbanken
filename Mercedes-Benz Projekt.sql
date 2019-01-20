f4ccf3b2d63a2be071a5b23279defe98c6366753
rithbuster <denis.bauer1@gmx.net>
2019-01-20 21:08:10 +0100

    erweitert

--- Mercedes-Benz Projekt.sql
+++ Mercedes-Benz Projekt.sql
@@ -2,10 +2,12 @@ create table Mercedes_Benz_Projekt (
     FahrzeugID INTEGER,
     KundenID INTEGER,
     Auto varchar (40),
-    Baujahr varchar (40),
+    Baujahr INTEGER,
     Preis INTEGER,
     Aufbauform varchar (40),
     Verkaufszahl INTEGER,
     PRIMARY KEY (FahrzeugID)
 );
 
+insert into Mercedes_Benz_Projekt
+values (1,11,C-Klasse,Null,37.956.95,Coupé,1429);
