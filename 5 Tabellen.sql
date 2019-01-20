6e90b7c51aa4cb4873492af81027f9709a61da31
1Ole1 <46760194+1Ole1@users.noreply.github.com>
2019-01-20 19:59:39 +0100

    Create 5 Tabellen.sql

---
+++ 5 Tabellen.sql
@@ -0,0 +1,87 @@
+CREATE TABLE Filme
+(
+   Titel VARCHAR (100),
+   Genre VARCHAR (30),
+   USK INTEGER,
+   Regisseur VARCHAR (30),
+   Laenge INTEGER,
+   Veroeffentlichung DATE
+);
+
+CREATE TABLE Filme
+(
+   Titel VARCHAR (100),
+   Genre VARCHAR (30),
+   USK INTEGER,
+   Regisseur VARCHAR (30),
+   Laenge INTEGER,
+   Veroeffentlichung DATE
+);
+
+CREATE TABLE Filme
+(
+   Titel VARCHAR (100),
+   Genre VARCHAR (30),
+   USK INTEGER,
+   Regisseur VARCHAR (30),
+   Laenge INTEGER,
+   Veroeffentlichung DATE
+);
+
+CREATE TABLE Filme
+(
+   Titel VARCHAR (100),
+   Genre VARCHAR (30),
+   USK INTEGER,
+   Regisseur VARCHAR (30),
+   Laenge INTEGER,
+   Veroeffentlichung DATE
+);
+
+
+CREATE TABLE Filme
+(
+   Titel VARCHAR (100),
+   Genre VARCHAR (30),
+   USK INTEGER,
+   Regisseur VARCHAR (30),
+   Laenge INTEGER,
+   Veroeffentlichung DATE
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
