73bbb73480c2dfabfc5760110d4e128154bdf287
rithbuster <denis.bauer1@gmx.net>
2019-01-20 20:08:00 +0100

    Grundgerüst

--- Mercedes-Benz Projekt.sql
+++ Mercedes-Benz Projekt.sql
@@ -1,8 +1,10 @@
 create table Mercedes_Benz_Projekt (
     AutoID INTEGER,
-    Autos varchar (40),
+    Auto varchar (40),
     Baujahr varchar (40),
     Preis INTEGER,
     Aufbauform varchar (40),
-    test INTEGER,
-)
+    Verkaufszahl INTEGER,
+    PRIMARY KEY (AutoID),
+);
+
