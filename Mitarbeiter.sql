fb24db0f29d07a5ae9aab70a4f4e78ec8758dcc7
1Ole1 <46760194+1Ole1@users.noreply.github.com>
2019-01-23 17:59:13 +0100

    Update Mitarbeiter.sql

--- Mitarbeiter.sql
+++ Mitarbeiter.sql
@@ -10,3 +10,43 @@ CREATE TABLE Mitarbeiter
    PRIMARY KEY (MitarbeiterID)
 );
 
+insert into Mitarbeiter
+values (1,Peter,Mueller,3,5000,1965);
+insert into Mitarbeiter
+values (2,Michael,Schmidt,4,6800,1971);
+insert into Mitarbeiter
+values (3,Thomas,Schneider,1,7000,1969);
+insert into Mitarbeiter 
+values (4,Wolfgang,Fischer,3,5000,1974);
+insert into Mitarbeiter 
+values (5,Anderas,Weber,1,7000,1983);
+insert into Mitarbeiter 
+values (6,Klaus,Meyer,3,5000,1979);
+insert into Mitarbeiter 
+values (7,Juergen,Wagner,1,7000,1963);
+insert into Mitarbeiter 
+values (8,Guenter,Becker,5,5000,1977);
+insert into Mitarbeiter 
+values (9,Stefan,Schulz,2,5500,1995);
+insert into Mitarbeiter
+values (10,Uwe,Schaefer,4,6800,1978);
+insert into Mitarbeiter
+values (11,Christian,Koch,4,6800,1988);
+insert into Mitarbeiter
+values (12,Werner,Bauer,5,5000,1999);
+insert into Mitarbeiter
+values (13,Horst,Richter,2,5500,1986);
+insert into Mitarbeiter 
+values (14,Frank,Klein,4,6800,1976);
+insert into Mitarbeiter 
+values (15,Dieter,Wolf,6,6600,1985);
+insert into Mitarbeiter 
+values (16,Manfred,Schroeder,3,5000,1975);
+insert into Mitarbeiter 
+values (17,Gerhard,Neumann,6,6600,1981);
+insert into Mitarbeiter 
+values (18,Hans,Schwarz,2,5500,1989);
+insert into Mitarbeiter 
+values (19,Bernd,Zimmermann,5,5000,1966);
+insert into Mitarbeiter
+values (20,Thorsten,Braun,6,6600,1987);
