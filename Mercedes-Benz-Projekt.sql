a3678a2ac7b06cf440ec8ab6c0611fa899945c7b
1Ole1 <46760194+1Ole1@users.noreply.github.com>
2019-01-23 12:00:15 +0100

    Update Mercedes-Benz-Projekt.sql

--- Mercedes-Benz-Projekt.sql
+++ Mercedes-Benz-Projekt.sql
@@ -1,6 +1,6 @@
 create table Mercedes_Benz_Projekt (
     FahrzeugID INTEGER,
-    KundenID INTEGER,
+    KundenID INTEGER,18515
     Auto varchar (40),
     Baujahr INTEGER,
     Preis INTEGER,
@@ -150,45 +150,45 @@ values (5,Coupe);
 insert into Abteilung
 values (6,Sport);
 insert into Mitarbeiter
-values (1,Peter,Mueller,3);
+values (1,Peter,Mueller,3,5000,1965);
 insert into Mitarbeiter
-values (2,Michael,Schmidt,4);
+values (2,Michael,Schmidt,4,6800,1971);
 insert into Mitarbeiter
-values (3,Thomas,Schneider,1);
+values (3,Thomas,Schneider,1,7000,1969);
 insert into Mitarbeiter 
-values (4,Wolfgang,Fischer,3);
+values (4,Wolfgang,Fischer,3,5000,1974);
 insert into Mitarbeiter 
-values (5,Anderas,Weber,1);
+values (5,Anderas,Weber,1,7000,1983);
 insert into Mitarbeiter 
-values (6,Klaus,Meyer,3);
+values (6,Klaus,Meyer,3,5000,1979);
 insert into Mitarbeiter 
-values (7,Juergen,Wagner,1);
+values (7,Juergen,Wagner,1,7000,1963);
 insert into Mitarbeiter 
-values (8,Guenter,Becker,5);
+values (8,Guenter,Becker,5,5000,1977);
 insert int Mitarbeiter 
-values (9,Stefan,Schulz,2);
+values (9,Stefan,Schulz,2,5500,1995);
 insert into Mitarbeiter
-values (10,Uwe,Schaefer,4);
+values (10,Uwe,Schaefer,4,6800,1978);
 insert into Mitarbeiter
-values (11,Christian,Koch,4);
+values (11,Christian,Koch,4,6800,1988);
 insert into Mitarbeiter
-values (12,Werner,Bauer,5);
+values (12,Werner,Bauer,5,5000,1999);
 insert into Mitarbeiter
-values (13,Horst,Richter,2);
+values (13,Horst,Richter,2,5500,1986);
 insert into Mitarbeiter 
-values (14,Frank,Klein,4);
+values (14,Frank,Klein,4,6800,1976);
 insert into Mitarbeiter 
-values (15,Dieter,Wolf,6);
+values (15,Dieter,Wolf,6,6600,1985);
 insert into Mitarbeiter 
-values (16,Manfred,Schroeder,3);
+values (16,Manfred,Schroeder,3,5000,1975);
 insert into Mitarbeiter 
-values (17,Gerhard,Neumann,6);
+values (17,Gerhard,Neumann,6,6600,1981);
 insert into Mitarbeiter 
-values (18,Hans,Schwarz,2);
+values (18,Hans,Schwarz,2,5500,1989);
 insert into Mitarbeiter 
-values (19,Bernd,Zimmermann,5);
+values (19,Bernd,Zimmermann,5,5000,1966);
 insert into Mitarbeiter
-values (20,Thorsten,Braun,6);
+values (20,Thorsten,Braun,6,6600,1987);
 
 
 Create View Abfrage1 as (Select * From Mercedes_Benz_Projekt Where Baujahr = 2017 and Aufbauform = SUV
