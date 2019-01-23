0a655d50ea65bf6aeb01bf36177b306f64d2ded1
1Ole1 <46760194+1Ole1@users.noreply.github.com>
2019-01-23 17:32:39 +0100

    Update Mercedes-Benz-Projekt.sql

--- Mercedes-Benz-Projekt.sql
+++ Mercedes-Benz-Projekt.sql
@@ -16,6 +16,8 @@ CREATE TABLE Maschinen
    HerstellungsID NUMERIC,
    Zustaendiger_Mitarbeiter VARCHAR (40),
    Herstelldatum DATE
+    
+    PRIMARY KEY (MaschinenID)
 );
 
 CREATE TABLE Kunden
@@ -98,25 +100,25 @@ values  (23,230,E-Klasse_T-Modell,2015,4824855,T-Modell,1239);
 insert into Mercedes_Benz_Projekt
 values  (24,240,E-Klasse_All_terrain,2016,5828025,SUV,9272);
 insert into Maschinen
-values (1,Auspuffanlage,1453,);
+values (1,Auspuffanlage,1453,Peter_Mueller,6-3-18);
 insert into Maschinen
-values (2,Kuehlung,3245,);
+values (2,Kuehlung,3245,Michael_Schmidt,2-6-17);
 insert into Maschinen
-values (3,Kuplung,8567,);
+values (3,Kuplung,8567,Thomas_Schneider,8-5-18);
 insert into Maschinen
-values (4,Achsantrieb,7403,);
+values (4,Achsantrieb,7403,Wolfgang_Fischer,5-1-19);
 insert into Maschinen 
-values (5,Karosserie,8763,);
+values (5,Karosserie,8763,Andreas_Weber,4-1-19);
 insert into Maschinen 
-values (6,Bremsanlage,7197,);
+values (6,Bremsanlage,7197,Klaus_Meyer,5-8-19);
 insert into Maschinen 
-values (7,Stossdaempfer,1495,);
+values (7,Stossdaempfer,1495,Juergen_Wagner,22-2-19);
 insert into Maschinen
-values (8,Riementrieb,6573,);
+values (8,Riementrieb,6573,Guenter_Becker,13-3-19);
 insert into Maschinen 
-values (9,Felgen,6582,);
+values (9,Felgen,6582,Stefan_Schulz,5-3-18);
 insert into Maschinen 
-values (10,Reifen,3259,);
+values (10,Reifen,3259,Christian_Koch,8-8-17);
 insert into Kunden 
 values (22,C-Klasse,Brandt,7-11-18);
 insert into Kunden
