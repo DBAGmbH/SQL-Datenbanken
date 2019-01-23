b0d8ad6b9621d5d9cc96c9d20d00b059d33d9cfe
rithbuster <denis.bauer1@gmx.net>
2019-01-23 19:00:49 +0100

    ka

--- Kunden.sql
+++ Kunden.sql
@@ -1,6 +1,5 @@
-CREATE TABLE Kunden
-(
-   KundenID VARCHAR (40),
+CREATE TABLE Kunden (
+   KundenID INTEGER,
    Einkaeufe VARCHAR (40),
    Nachname VARCHAR (40),
    Bestelldatum DATE
@@ -10,22 +9,22 @@ CREATE TABLE Kunden
 
 
 insert into Kunden 
-values (22,C-Klasse,Brandt,7-11-18);
+values (22,'C-Klasse','Brandt',7-11-18);
 insert into Kunden
-values (33,B-Klasse,Heinrich,3-8-17);
+values (33,'B-Klasse','Heinrich',3-8-17);
 insert into Kunden
-values (44,E-Klasse_T-Modell,Schulte,11-12-16);
+values (44,'E-Klasse_T-Modell','Schulte',11-12-16);
 insert into Kunden 
-values (55,CLA,Schreiber,26-1-19);
+values (55,'CLA','Schreiber',26-1-19);
 insert into Kunden 
-values (66,GLS_SUV,Dietrich,25-6-18);
+values (66,'GLS_SUV','Dietrich',25-6-18);
 insert into Kunden 
-values (77,CLS,Sauer,13-8-17);
+values (77,'CLS','Sauer',13-8-17);
 insert into Kunden 
-values (88,S-Klasse_Long,Busch,22-9-16);
+values (88,'S-Klasse_Long','Busch',22-9-16);
 insert into Kunden 
-values (99,A-Klasse,Bergmann,31-3-17);
+values (99,'A-Klasse','Bergmann',31-3-17);
 insert into Kunden 
-values (111,GLC_SUV,Feiffer,28-4-18);
+values (111,'GLC_SUV','Feiffer',28-4-18);
 insert into Kunden 
-values (122,Maybach_S-Klasse,Ziegler,29-2-18);
+values (122,'Maybach_S-Klasse','Ziegler',29-2-18);
