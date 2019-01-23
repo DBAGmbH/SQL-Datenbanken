bcad220b7c07ab2a642ceb56fada86914416d6f1
1Ole1 <46760194+1Ole1@users.noreply.github.com>
2019-01-23 10:34:39 +0100

    Update Mercedes-Benz-hauptdatei.sql

--- Mercedes-Benz-hauptdatei.sql
+++ Mercedes-Benz-hauptdatei.sql
@@ -10,6 +10,46 @@ create table Mercedes_Benz_Projekt (
     FOREIGN KEY Abteilung(AbteilungsID)
 );
 
+CREATE TABLE Maschinen
+(
+   MaschinenID VARCHAR (40),
+   Herstellung VARCHAR (40),
+   HerstellungsID NUMERIC,
+   Zustaendiger_Mitarbeiter VARCHAR (40),
+   Herstelldatum DATE
+);
+
+CREATE TABLE Kunden
+(
+   KundenID VARCHAR (40),
+   Einkaeufe VARCHAR (40),
+   Nachname VARCHAR (40),
+   Bestelldatum DATE
+   
+   PRIMARY KEY (KundenID)
+);
+
+CREATE TABLE Abteilung
+(
+   AbteilungsID VARCHAR (40),
+   Zustaendigkeit VARCHAR (40),
+   
+   PRIMARY KEY (AbteilungsID)
+      
+);
+
+CREATE TABLE Mitarbeiter
+(
+   MitarbeiterID VARCHAR (40),
+   Vorname VARCHAR (40),
+   Name INTEGER,
+   Abteilung VARCHAR (40),
+   Einkommen INTEGER,
+   Geburtsjahr DATE
+   
+   PRIMARY KEY (MitarbeiterID)
+);
+
 insert into Mercedes_Benz_Projekt
 values (1,11,C-Klasse,2016,3795695,Coupé,14729);
 insert into Mercedes_Benz_Projekt
