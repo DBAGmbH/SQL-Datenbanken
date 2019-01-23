631c47901fba18a5cda56690e3f6e1a1f8a94521
1Ole1 <46760194+1Ole1@users.noreply.github.com>
2019-01-23 10:32:54 +0100

    Update Mercedes-Benz-hauptdatei.sql

--- Mercedes-Benz-hauptdatei.sql
+++ Mercedes-Benz-hauptdatei.sql
@@ -1,1 +1,60 @@
+create table Mercedes_Benz_Projekt (
+    FahrzeugID INTEGER,
+    KundenID INTEGER,
+    Auto varchar (40),
+    Baujahr INTEGER,
+    Preis INTEGER,
+    Aufbauform varchar (40),
+    Verkaufszahl INTEGER,
+    PRIMARY KEY (FahrzeugID)
+    FOREIGN KEY Abteilung(AbteilungsID)
+);
 
+insert into Mercedes_Benz_Projekt
+values (1,11,C-Klasse,2016,3795695,Coupé,14729);
+insert into Mercedes_Benz_Projekt
+values (2,22,CLA,2016,3058300,Coupé,18120);
+insert into Mercedes_Benz_Projekt
+values (3,33,E-Klasse,2019,4893280,Coupé,8752);
+insert into Mercedes_Benz_Projekt
+values (4,44,GLC,2018,5049170,Coupé,1942);
+insert into Mercedes_Benz_Projekt
+values (5,55,GLE,2017,7169750,Coupé,21384);
+insert into Mercedes_Benz_Projekt
+values (6,66,S-Klasse,2016,10183425,Coupé,14246);
+insert into Mercedes_Benz_Projekt
+values (7,77,AMG_GT,2017,9525950,Coupé,6283);
+insert into Mercedes_Benz_Projekt
+values (8,88,G-Klasse,2019,10704050,SUV,10203);
+insert into Mercedes_Benz_Projekt
+values (9,99,GLA_SUV,2018,2985710,SUV,18492);
+insert into Mercedes_Benz_Projekt
+values (10,100,GLC_SUV,2016,4596970,SUV,2840);
+insert into Mercedes_Benz_Projekt
+values (11,110,GLE_SUV,2015,6580700,SUV,19823);
+insert into Mercedes_Benz_Projekt
+values (12,120,GLS_SUV,2017,7973595,SUV,20192);
+insert into Mercedes_Benz_Projekt
+values  (13,130,A-Klasse,2016,2609075,Limousine,1237);
+insert into Mercedes_Benz_Projekt
+values  (14,140,CLS,2018,6057100,Coupé,2739);
+insert into Mercedes_Benz_Projekt
+values  (15,150,B-Klasse,2017,3187115,Sport,29147);
+insert into Mercedes_Benz_Projekt
+values  (16,160,A-Klasse,2018,3091620,Limousine,123897);
+insert into Mercedes_Benz_Projekt
+values  (17,170,C-Klasse,2018,3253460,Limousine,12839);
+insert into Mercedes_Benz_Projekt
+values  (18,180,E-Klasse,2018,4486300,Limousine,7747);
+insert into Mercedes_Benz_Projekt
+values  (19,190,S-Klasse,2017,2576925,Limousine,12890);
+insert into Mercedes_Benz_Projekt
+values  (20,200,S-Klasse_Long,2018,8904175,Limousine,4728);
+insert into Mercedes_Benz_Projekt
+values  (21,210,Maybach_S-Klasse,2016,14083055,Limousine,62398);
+insert into Mercedes_Benz_Projekt
+values  (22,220,C-Klasse_T-Modell,2019,3420060,T-Modell,52378);
+insert into Mercedes_Benz_Projekt
+values  (23,230,E-Klasse_T-Modell,2015,4824855,T-Modell,1239);
+insert into Mercedes_Benz_Projekt
+values  (24,240,E-Klasse_All_terrain,2016,5828025,SUV,9272);
